a7cd63c2eeaef8e0a4b7f8492c20a6d819758529
Balint <rueb@campus.tu-berlin.de>
2022-11-18 14:55:21 +0100

    added function to parse Services from Service List

--- dvbi_lib/lib/dvbi_lib.dart
+++ dvbi_lib/lib/dvbi_lib.dart
@@ -1,7 +1,33 @@
 library dvbi_lib;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:convert';
+import 'dart:io';
+import 'package:xml/xml.dart' as xml;
+import 'package:xml/xml_events.dart';
+
+class XMLParser {
+  String url = "https://dvb-i.net/production/services.php/de";
+
+  Future<List<xml.XmlNode>> getServicesFromXML () async {
+    // produces a request object
+    var request = await HttpClient().getUrl(Uri.parse(url));
+    // sends the request
+    var response = await request.close();
+    // transforms and prints the response
+    return await response
+      .transform(utf8.decoder)
+      .toXmlEvents()
+      .selectSubtreeEvents(((event) => event.name =="Service"))
+      .toXmlNodes()
+      .flatten()
+      .toList();
+    }
+
+}
+
+Future<void> main() async {
+  XMLParser parser = XMLParser();
+  List<xml.XmlNode> services = await parser.getServicesFromXML();
+  print(services.length);
+
 }
