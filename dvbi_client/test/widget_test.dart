dfa22f0baf5d61a5d807ed5f3793b16cfa892cd2
NiklasCarlos1994 <nhaarbeck@yahoo.de>
2022-11-28 12:43:39 +0100

    video_player added with ard live stream - works only on android

--- dvbi_client/test/widget_test.dart
+++ dvbi_client/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:dvbi_client/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const VideoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
