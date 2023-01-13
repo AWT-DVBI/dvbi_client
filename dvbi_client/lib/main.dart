7f565b7dba2f108f5df76a175fb97e25920045ba
Luis Hebendanz <luis@qube.email>
2023-01-13 20:03:34 +0100

    Init radical change

--- dvbi_client/lib/main.dart
+++ dvbi_client/lib/main.dart
@@ -1,46 +1,20 @@
 // ignore_for_file: unused_import
 
 import 'package:flutter/material.dart';
-import 'video_carousel.dart';
 import 'package:dvbi_lib/dvbi_lib.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:developer' as dev;
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:logger/logger.dart';
-// Code generation
 import 'package:flutter/foundation.dart';
+import 'package:dvbi_client/app/app.dart';
 
+// globals
 const String endpointUrl = "https://dvb-i.net/production/services.php/de";
-
-final dvbiProvider = FutureProvider.autoDispose((ref) async {
-  //String data = await rootBundle.loadString("assets/services.xml");
-
-  final dvbi = await DVBI().initialize(endpointUrl: Uri.parse(endpointUrl));
-
-  return dvbi.serviceElems;
-});
-
 var logger = Logger(printer: PrettyPrinter());
-
 var loggerNoStack = Logger(printer: PrettyPrinter(methodCount: 0));
 
 void main() {
   runApp(
-    // For widgets to be able to read providers, we need to wrap the entire
-    // application in a "ProviderScope" widget.
-    // This is where the state of our providers will be stored.
-    const ProviderScope(
-      child: MyApp(),
-    ),
+    const ChewieDemo(),
   );
-}
-
-// Extend ConsumerWidget instead of StatelessWidget, which is exposed by Riverpod
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const MaterialApp(home: VideoCarousel());
-  }
 }
